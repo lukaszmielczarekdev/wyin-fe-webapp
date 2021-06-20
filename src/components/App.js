7b7080e7877762c099fa62db2df26df9c3777298
Jakub Kolodziejczak <jakub.kolodziejczak@postgrad.put.poznan.pl>
2021-06-20 19:58:04 +0200

    WYIN-181: add toast message when content was copied

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import MainSection from "./mainSection";
 import NavBar from "./navBar";
 import Footer from "./footer";
 import About from "./about";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 export default function App() {
@@ -17,6 +19,17 @@ export default function App() {
           <Route path="/" component={MainSection} />
         </Switch>
       </main>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={3000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+      />
       <Footer />
     </div>
   );

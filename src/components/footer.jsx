ff541583781b7b127a95ae71507fe251d13229c8
Jakub Kolodziejczak <jakub.kolodziejczak@postgrad.put.poznan.pl>
2021-06-22 23:18:21 +0200

    WYIN-203: show version in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import "./footer.css";
 
-const Footer = () => {
-  return (
-    <div className="footer">
-      <h2>Copyright &copy; 2021 WYIN</h2>
-    </div>
-  );
+class Footer extends Component {
+  renderVersion() {
+    return (
+      <h2>&nbsp;version: {process.env.REACT_APP_VERSION}</h2>
+    );
+  }
+
+  render() {
+    return (
+      <div className="footer">
+        <h2>Copyright &copy; 2021 WYIN</h2>
+        {process.env.REACT_APP_VERSION && this.renderVersion()}
+      </div>
+    );
+  }
 };
 
 export default Footer;

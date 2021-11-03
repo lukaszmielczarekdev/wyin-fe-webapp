eb1896a70cd592d2a8ebf03ea96c0c59b37a6693
mrl5 <31549762+mrl5@users.noreply.github.com>
2021-11-03 19:40:34 +0100

    feat: use SDK instead of API [#3]
    BREAKING CHANGE: It's not using backend anymore
    
    closes #2, #3

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,54 +1,20 @@
+import * as wyinSDK from '@spio-wyin/wyin-sdk-feed';
+
 const api = {
   getHistoryEvents: async (time) => {
-    const params = { t: time };
-    const endpoint = "/history/event";
-    const qs = getQueryString(params);
-    const url = qs.length > 0 ? `${endpoint}?${qs}` : endpoint;
-    const base = process.env.REACT_APP_WYIN_BE_FEED_API;
-
-    const uri = new URL(url, base);
-    return getApiResponse(uri);
+    const lang = 'pl';
+    return wyinSDK.getEventByTime(time, lang);
   },
 
   getHistoryRandomEvent: async () => {
-    const endpoint = "/history/event/random";
-    const base = process.env.REACT_APP_WYIN_BE_FEED_API;
-
-    const uri = new URL(endpoint, base);
-    return getApiResponse(uri);
+    const lang = 'pl';
+    return wyinSDK.getEventByRandom(lang);
   },
 
   getHistoryYearEvent: async (year) => {
-    const endpoint = `/history/event/${year}`;
-    const base = process.env.REACT_APP_WYIN_BE_FEED_API;
-
-    const uri = new URL(endpoint, base);
-    return getApiResponse(uri);
+    const lang = 'pl';
+    return wyinSDK.getEventByYear(parseInt(year, 10), lang);
   },
 };
 
 export default api;
-
-async function getApiResponse(uri) {
-  const response = await fetch(uri);
-  throwOnErrorStatusCode(response.status);
-  return response.json();
-}
-
-function getQueryString(params) {
-  const queryStrings = Object.entries(params).map((p) => {
-    const [key, value] = p;
-    return `${key}=${encodeURIComponent(value)}`;
-  });
-  return queryStrings.join("&");
-}
-
-function throwOnErrorStatusCode(statusCode) {
-  if (statusCode === 404) {
-    return false;
-  } else if (statusCode >= 400) {
-    throw new Error(`API responded with: ${statusCode}`);
-  } else if (!statusCode) {
-    throw new Error("Woops, there is no status code");
-  }
-}
